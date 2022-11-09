516c541dd52641a58aa01f5ce548ca451a632d3f
Jeremy Berman <jerber@sas.upenn.edu>
2022-11-09 16:38:35 -0500

    use straw defaults by default

--- edge_orm/unset.py
+++ edge_orm/unset.py
@@ -1,32 +1,31 @@
 from typing import Any, Optional, Type
 
+try:
+    from strawberry.unset import UNSET, UnsetType
+except ImportError:
 
-class UnsetType:
-    """Taken from Strawberry: https://strawberry.rocks/"""
+    class UnsetType:  # type: ignore
+        """Taken from Strawberry: https://strawberry.rocks/"""
 
-    __instance: Optional["UnsetType"] = None
+        __instance: Optional["UnsetType"] = None
 
-    def __new__(cls: Type["UnsetType"]) -> "UnsetType":
-        if cls.__instance is None:
-            ret = super().__new__(cls)
-            cls.__instance = ret
-            return ret
-        else:
-            return cls.__instance
+        def __new__(cls: Type["UnsetType"]) -> "UnsetType":
+            if cls.__instance is None:
+                ret = super().__new__(cls)
+                cls.__instance = ret
+                return ret
+            else:
+                return cls.__instance
 
-    def __str__(self) -> str:
-        return ""
+        def __str__(self) -> str:
+            return ""
 
-    def __repr__(self) -> str:
-        return "UNSET"
+        def __repr__(self) -> str:
+            return "UNSET"
 
-    def __bool__(self) -> bool:
-        return False
+        def __bool__(self) -> bool:
+            return False
 
-
-UNSET: Any = UnsetType()
+    UNSET: Any = UnsetType()  # type: ignore
 
 __all__ = ["UNSET", "UnsetType"]
-
-# UNSET = "*_~_**"
-# __all__ = ["UNSET"]
